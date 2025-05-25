654a9d62852bd96c45a79047e7b370a570f220d7
Suzan1345 <Suzan.Korkmaz@Student.HTW-Berlin.de>
2025-05-25 09:32:12 +0200

    Frontend

---
+++ Front/src/components/Booklist.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(book, index) in books" :key="index">
+  {{ book.title }} von {{ book.author }}
+</li>
+</ul>
+</div>
+</template>
+
+<script setup lang="ts">
+  // Props typisieren
+  interface Book {
+  title: string
+  author: string
+}
+
+// defineProps ist die neue Art Props in <script setup> zu definieren
+const props = defineProps<{
+  books: Book[]
+}>()
+</script>

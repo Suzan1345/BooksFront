809d8276b539fccd8f0bb4233bd8607199d2abc0
Suzan1345 <Suzan.Korkmaz@Student.HTW-Berlin.de>
2025-09-14 09:58:35 +0200

    bild

--- Front/src/views/HomeView.vue
+++ Front/src/views/HomeView.vue
@@ -3,7 +3,7 @@
 
 
 
-<img src='@/components/pics/placeholder.png' />
+<img src='@/assets/pics/placeholder.png' />
 </template>
 <script setup lang="ts">
 import Header from '@/components/Header.vue'
